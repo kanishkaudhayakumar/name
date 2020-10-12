b3baf424e011e55539159d8b02486bae5436cd8f
kanishudhya <kanishudhya@gmail.com>
2020-10-12 06:20:32 -0700

    first commit

---
+++ make1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class make1{
+	public static void main(String args[])
+	{
+		System.out.println("welcome all");
+		System.out.println("hello world");
+		System.out.println("helllooooo welcome");
+		System.out.println("welcome to github");
+ System.out.println("welcome to git");	}
+}
+
+
